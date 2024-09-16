63ab2ee344d278b6e16ce9a26973350d221b4af2
Slay Stina <august.hogbom@gmail.com>
2024-09-16 13:19:19 +0200

    Addade C/F konverterare

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-
-
-
             int nr1 = 123;
             int nr2 = 321;
             Console.WriteLine(nr1 + nr2);
 
+            Console.Write("Vad heter du? ");
             string firstname = Console.ReadLine();
+            Console.Write("Vart bor du? ");
             string city = Console.ReadLine();
+            Console.Write("Hur gammal är du? ");
             string age = Console.ReadLine();
 
             Console.WriteLine("Jag heter " + firstname + " ,jag bor i " + city + " och är " + age + " år gammal!");
@@ -20,7 +20,7 @@
             Console.WriteLine($"Jag heter {firstname}, jag bor i {city} och är {age} år gammal!");
 
             Console.BackgroundColor = ConsoleColor.DarkGreen;
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Red;
 
             for (int i = 0; i < 11; i++)
             {
@@ -37,9 +37,17 @@
 
             Console.Write("Ange din vikt i kilo: ");
             int weight = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Du väger {weight * 1000} gram!");
+            Console.WriteLine($"Du väger {weight * 1000} gram!\n");
 
-            Console.WriteLine("Idag är det " + DateTime.Now);
+            Console.WriteLine("Idag är det " + DateTime.Now + "\n");
+
+            Console.WriteLine("Hur varmt är det i farenheight?");
+            Console.Write("Ange temperatur i celsius: ");
+            int celsius = int.Parse(Console.ReadLine());
+
+            double fht = celsius * 1.8 + 32;
+            Console.WriteLine($"{celsius} grader i celsius konverteras till {fht} grader i farenheight");
+
         }
     }
 }
