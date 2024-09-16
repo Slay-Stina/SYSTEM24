c5b7af95ee8cd4b5b00c538729d2678217da2525
Slay Stina <august.hogbom@gmail.com>
2024-09-16 12:48:02 +0200

    Har har man lärt sig saker, dags att lära sig mer i guess

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+            int nr1 = 123;
+            int nr2 = 321;
+            Console.WriteLine(nr1 + nr2);
+
+            string firstname = Console.ReadLine();
+            string city = Console.ReadLine();
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Jag heter " + firstname + " ,jag bor i " + city + " och är " + age + " år gammal!");
+            Console.WriteLine(string.Format("Jag heter {0}, jag bor i {1} och är {2} år gammal!", firstname, city, age));
+            Console.WriteLine($"Jag heter {firstname}, jag bor i {city} och är {age} år gammal!");
+
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write($" {firstname} HAR EN BOMB! ");
+                Console.Beep();
+            }
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("Ange din favoriträtt: ");
+            string faveDish = Console.ReadLine();
+
+            Console.Write("Ange din vikt i kilo: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Du väger {weight * 1000} gram!");
+
+            Console.WriteLine("Idag är det " + DateTime.Now);
         }
     }
 }
