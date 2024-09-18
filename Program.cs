fa3f1ae2d2b7243d0426e43dcd8df423556fa0da
Slay Stina <august.hogbom@gmail.com>
2024-09-18 13:06:55 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -86,42 +86,42 @@ namespace ConsoleApp1
                     break;
             }
 
-            //    Console.Write("Vad heter du? ");
-            //    string firstname = Console.ReadLine();
-            //    Console.Write("Vart bor du? ");
-            //    string city = Console.ReadLine();
-            //    Console.Write("Hur gammal är du? ");
-            //    string age = Console.ReadLine();
+            Console.Write("Vad heter du? ");
+            string firstname = Console.ReadLine();
+            Console.Write("Vart bor du? ");
+            string city = Console.ReadLine();
+            Console.Write("Hur gammal är du? ");
+            string age = Console.ReadLine();
 
-            //    Console.WriteLine("Jag heter " + firstname + " ,jag bor i " + city + " och är " + age + " år gammal!");
-            //    Console.WriteLine(string.Format("Jag heter {0}, jag bor i {1} och är {2} år gammal!", firstname, city, age));
-            //    Console.WriteLine($"Jag heter {firstname}, jag bor i {city} och är {age} år gammal!");
+            Console.WriteLine("Jag heter " + firstname + " ,jag bor i " + city + " och är " + age + " år gammal!");
+            Console.WriteLine(string.Format("Jag heter {0}, jag bor i {1} och är {2} år gammal!", firstname, city, age));
+            Console.WriteLine($"Jag heter {firstname}, jag bor i {city} och är {age} år gammal!");
 
-            //    Console.BackgroundColor = ConsoleColor.DarkGreen;
-            //    Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Red;
 
-            //    for (int i = 0; i < 11; i++)
-            //    {
-            //        Console.Write($" {firstname} HAR EN BOMB! ");
-            //        Console.Beep();
-            //    }
-            //    Console.WriteLine();
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write($" {firstname} HAR EN BOMB! ");
+                Console.Beep();
+            }
+            Console.WriteLine();
 
-            //    Console.BackgroundColor = ConsoleColor.Black;
-            //    Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
 
-            //    Console.Write("Ange din favoriträtt: ");
-            //    string faveDish = Console.ReadLine();
+            Console.Write("Ange din favoriträtt: ");
+            string faveDish = Console.ReadLine();
 
-            //    Console.Write("Ange din vikt i kilo: ");
-            //    int weight = int.Parse(Console.ReadLine());
-            //    Console.WriteLine($"Du väger {weight * 1000} gram!\n");
+            Console.Write("Ange din vikt i kilo: ");
+            int weight = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Du väger {weight * 1000} gram!\n");
 
-            //    Console.WriteLine("Idag är det " + DateTime.Now + "\n");
+            Console.WriteLine("Idag är det " + DateTime.Now + "\n");
 
-            //    Console.WriteLine("Hur varmt är det i farenheight?");
-            //    Console.Write("Ange temperatur i celsius: ");
-            //    int celsius = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur varmt är det i farenheight?");
+            Console.Write("Ange temperatur i celsius: ");
+            int celsius = int.Parse(Console.ReadLine());
 
             double fht = celsius * 1.8 + 32;
             Console.WriteLine($"{celsius} grader i celsius konverteras till {fht} grader i farenheight\n");
